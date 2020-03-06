90eba699d683aac18fa0483d958f54b222d9545f
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-03-06 16:43:38 -0700

    initial commit

---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,97 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	private int id;
+	private double replacementCost;
+	private String rating;
+	private Integer releaseYear;
+	private String specialFeatures;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public Film(int id, double replacementCost, String rating, Integer releaseYear, String specialFeatures) {
+		super();
+		this.id = id;
+		this.replacementCost = replacementCost;
+		this.rating = rating;
+		this.releaseYear = releaseYear;
+		this.specialFeatures = specialFeatures;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Film [id=");
+		builder.append(id);
+		builder.append(", replacementCost=");
+		builder.append(replacementCost);
+		builder.append(", rating=");
+		builder.append(rating);
+		builder.append(", releaseYear=");
+		builder.append(releaseYear);
+		builder.append(", specialFeatures=");
+		builder.append(specialFeatures);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
+		result = prime * result + ((releaseYear == null) ? 0 : releaseYear.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(replacementCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((specialFeatures == null) ? 0 : specialFeatures.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		if (id != other.id)
+			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
+			return false;
+		if (releaseYear == null) {
+			if (other.releaseYear != null)
+				return false;
+		} else if (!releaseYear.equals(other.releaseYear))
+			return false;
+		if (Double.doubleToLongBits(replacementCost) != Double.doubleToLongBits(other.replacementCost))
+			return false;
+		if (specialFeatures == null) {
+			if (other.specialFeatures != null)
+				return false;
+		} else if (!specialFeatures.equals(other.specialFeatures))
+			return false;
+		return true;
+	}
+	
+}
